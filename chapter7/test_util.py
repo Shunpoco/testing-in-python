2f049afb55be512849cb86ddd1fcdefe16cc5d80
Shunpoco <tkngsnsk313320@gmail.com>
2022-08-06 17:32:07 +0900

    falure test

--- chapter7/test_util.py
+++ chapter7/test_util.py
@@ -3,16 +3,22 @@ import pytest
 import util
 
 class FakeResponse:
-    def __init__(self, status=200):
+    def __init__(self, status=200, body=""):
         self.status = status
+        self.body = body
 
 @pytest.fixture
 def response():
-    def apply(status=200):
-        return FakeResponse(status=status)
+    def apply(status=200, body=""):
+        return FakeResponse(status=status, body=body)
 
     return apply
 
 def test_build_message_success(response):
     result = util.build_message(response())
     assert result["success"] is True
+
+def test_build_message_failure(response):
+    result = util.build_message(response(status=400))
+
+    assert result["success"] is False
