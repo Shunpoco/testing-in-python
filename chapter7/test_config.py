b5c28fed1c159ec587dca0a6dd78b4a022209c66
Shunpoco <tkngsnsk313320@gmail.com>
2022-08-07 15:27:50 +0900

    fixture depends on fixture

--- chapter7/test_config.py
+++ chapter7/test_config.py
@@ -1,4 +1,20 @@
+import pytest
 import config
+
+@pytest.fixture
+def c(tmpdir):
+    template = """
+[main]
+verbose = {verbose_value}
+    """
+
+    def apply(verbose_value=True):
+        path = tmpdir.mkdir("etc").join("app.conf")
+        path.write(template.format(verbose_value=verbose_value))
+
+        return path.strpath
+
+    return apply
 
 def test_is_verbose_fails(tmpdir):
     path = tmpdir.mkdir("etc").join("app.conf")
@@ -6,14 +22,12 @@ def test_is_verbose_fails(tmpdir):
 
     assert config.is_verbose(path.strpath) is True
 
-def test_is_verbose_succeeds_false(tmpdir):
-    path = tmpdir.mkdir("etc").join("app.conf")
-    path.write("[main]\nverbose = 0")
+def test_is_verbose_succeeds_false(c):
+    config_path = c(verbose_value=False)
 
-    assert config.is_verbose(path.strpath) is False
+    assert config.is_verbose(config_path) is False
 
-def test_is_verbose_succeeds_true(tmpdir):
-    path = tmpdir.mkdir("etc").join("app.conf")
-    path.write("[main]\nverbose = 1")
+def test_is_verbose_succeeds_true(c):
+    config_path = c()
 
-    assert config.is_verbose(path.strpath) is True
+    assert config.is_verbose(config_path) is True
