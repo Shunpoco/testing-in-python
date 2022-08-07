1f9cbd5f8476a45f6365880ec1e518e9f9a87d33
Shunpoco <tkngsnsk313320@gmail.com>
2022-08-07 15:20:18 +0900

    config2

--- chapter7/test_config.py
+++ chapter7/test_config.py
@@ -5,3 +5,15 @@ def test_is_verbose_fails(tmpdir):
     path.write("[main]")
 
     assert config.is_verbose(path.strpath) is True
+
+def test_is_verbose_succeeds_false(tmpdir):
+    path = tmpdir.mkdir("etc").join("app.conf")
+    path.write("[main]\nverbose = 0")
+
+    assert config.is_verbose(path.strpath) is False
+
+def test_is_verbose_succeeds_true(tmpdir):
+    path = tmpdir.mkdir("etc").join("app.conf")
+    path.write("[main]\nverbose = 1")
+
+    assert config.is_verbose(path.strpath) is True
